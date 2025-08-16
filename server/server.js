7f7ffddb05a7280b4572b198bdf5d5bb8387f7c6
abdurrahman2004 <ar978874@gmail.com>
2025-08-16 23:08:44 +0530

    Fixed Connection issue

--- server/server.js
+++ server/server.js
@@ -13,7 +13,7 @@ const AdminRoute = require('./Routes/AdminRoute');
 const app = express();
 
 app.use(cors({
-  origin: ['https://pawpals-abdur.vercel.app/'],
+  origin: ['https://pawpals-abdur.vercel.app', 'http://localhost:5173'],
   credentials: true
 }));
 app.use(express.json());
